bc9e9f1f3b958a3c3c4f1232661602fe7f99ce4f
Navid Kabir <navid113@gmail.com>
2020-10-08 15:11:35 -0400

    Add rudimentary deeplink support using nc-deeplink. Needs frontend + options, however

--- server/functions/Analytics/link.js
+++ server/functions/Analytics/link.js
@@ -1,19 +1,29 @@
+import {DeepLinker} from "nc-deeplink";
+
 const mongoose = require('mongoose');
 const Link = mongoose.model('Link');
 const Visit = mongoose.model('Visit');
 
 module.exports = async (req, res) => {
-    let link, visit;
-    try {
-        link = await Link.findOne({_id: req.params._id });
-        visit = new Visit({
-            type: 'Link',
-            referral: req.params._id
-        });
-        visit = await visit.save();
-    } catch(err) {
-        return res.send(err.message);
-    } finally {
-        return res.redirect(link.url);
-    }
+  let link, visit;
+  try {
+    link = await Link.findOne({_id: req.params._id});
+    visit = new Visit({
+      type: 'Link',
+      referral: req.params._id
+    });
+    await visit.save();
+  } catch (err) {
+    return res.send(err.message);
+  }
+
+  if (link.useDeepLink) {
+    const userAgent = req.get('user-agent');
+    const deepLink = DeepLinker.parseDeepLink(link.url, userAgent);
+
+    return res.redirect(deepLink);
+  } else {
+    return res.redirect(link.url);
+  }
+
 }
