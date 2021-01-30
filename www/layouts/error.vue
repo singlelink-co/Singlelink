2a62cc1d182370053ac5653c2b888863e89f3be4
Jim Bisenius <jim@neutroncreative.com>
2021-01-29 22:56:06 -0500

    Add 404 page

---
+++ www/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <section class="mb-32 w-full">
+        <h1>404, Page not found!</h1>
+        <p class="mb-8">
+          Uh oh! Something doesn't look right. Maybe try again, or check out another page?
+        </p>
+        <div class="flex flex-col w-full lg:w-auto lg:flex-row items-center justify-start space-y-4 lg:space-y-0 lg:space-x-4">
+          <a class="text-center w-full lg:w-auto text-sm font-medium text-white px-6 py-6 lg:py-5 rounded-lg bg-indigo-600 hover:bg-indigo-500" href="/">Return home</a>
+          <a class="text-center w-full lg:w-auto text-sm font-medium text-gray-700 px-6 py-6 lg:py-5 rounded-lg bg-gray-300 hover:bg-gray-200" href="/gallery">View community examples</a>
+        </div>
+      </section>
+  </div>
+</tempate>
