702a9b10998b8b217c1b0f4d2dbd4e6c2599c8a7
Jim Bisenius <jim@neutroncreative.com>
2021-02-24 15:21:34 -0500

    Enriched marketplace data

--- client/components/theme/theme-list.vue
+++ client/components/theme/theme-list.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col w-full justify-center" :id="name.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, '').split(' ').join('-').toLowerCase()">
         <h2 class="font-bold text-xl py-2 border border-t-0 border-r-0 border-l-0 border-gray-200 w-full mb-2">{{ name }}</h2>
-        <div v-if="preview" class="flex flex-row flex-wrap mb-2 justify-start w-full">
+        <div v-if="!addon" class="flex flex-row flex-wrap mb-2 justify-start w-full">
             <!-- Show active theme first -->
             <a v-for="theme in themes" v-if="!icon && active == theme.id" :href="'/dashboard/marketplace/addon/'+theme.id+query_string" class="flex flex-col p-3 flex-1 hover:bg-indigo-200 bg-indigo-200 border border-indigo-600 rounded-lg" style="min-width:215px;max-width:235px;">
                 <theme :id="theme.id" :label="theme.label" :colors="theme.colors"/>
@@ -17,20 +17,20 @@
                 <theme :id="theme.id" :icon="icon" :label="theme.label" :colors="theme.colors"/>
             </div>
         </div>
-        <div v-if="!preview" class="flex flex-row flex-wrap mb-2 justify-start w-full">
+        <div v-if="addon" class="flex flex-row flex-wrap mb-2 justify-start w-full">
             <!-- Show active theme first -->
             <a v-for="theme in themes" v-if="!icon && active == theme.id" :href="'/dashboard/marketplace/addon/'+theme.id+query_string" class="flex flex-col p-3 flex-1 hover:bg-indigo-200 bg-indigo-200 border border-indigo-600 rounded-lg" style="min-width:215px;max-width:235px;">
-                <theme :id="theme.id" :label="theme.description" :preview="false"/>
+                <theme :colors="theme.resource.colors" :id="theme.id" :label="theme.displayName" />
             </a>
             <div v-for="theme in themes" v-if="icon && active == theme.id" @click="select_theme(theme.id)" class="flex flex-col p-3 flex-1 hover:bg-indigo-200 bg-indigo-200 border border-indigo-600 rounded-lg" style="min-width:215px;max-width:235px;">
-                <theme :id="theme.id" :icon="icon" :label="theme.description" :preview="false"/>
+                <theme :colors="theme.resource.colors" :id="theme.id" :icon="icon" :label="theme.displayName" />
             </div>
             <!-- List active themes -->
             <a v-for="theme in themes" v-if="!icon && active != theme.id" :href="'/dashboard/marketplace/addon/'+theme.id+query_string" class="flex flex-col p-3 flex-1 hover:bg-gray-200 rounded-lg" style="min-width:215px;max-width:235px;">
-                <theme :id="theme.id" :label="theme.description" :preview="false"/>
+                <theme :colors="theme.resource.colors" :id="theme.id" :label="theme.displayName" />
             </a>
             <div v-for="theme in themes" v-if="icon && active != theme.id" @click="select_theme(theme.id)" class="cursor-pointer flex flex-col p-3 flex-1 hover:bg-gray-200 rounded-lg" style="min-width:215px;max-width:235px;">
-                <theme :id="theme.id" :icon="icon" :label="theme.description" :preview="false"/>
+                <theme :colors="theme.resource.colors" :id="theme.id" :icon="icon" :label="theme.displayName" />
             </div>
         </div>
     </div>
@@ -46,9 +46,9 @@ export default {
         icon_click: String,
         item_click: String,
         active: String,
-        preview: {
+        addon: {
             type: Boolean,
-            default: true
+            default: false
         }
     },
     data() {
