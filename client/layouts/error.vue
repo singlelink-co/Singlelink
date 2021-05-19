b642ed95d8461a859f72f09ea268a200b3995273
Jim Bisenius <jim@neutroncreative.com>
2021-05-19 00:24:57 -0400

    Add 404 page

---
+++ client/layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="flex flex-col px-8 items-center overflow-x-hidden overflow-y-scroll py-16">
+    <div class="flex flex-row items-center justify-start mb-4 space-x-4 mt-8">
+      <img class="w-12" src="/Grimacing Face.svg"/>
+      <h1 class="text-black font-extrabold tracking-tight text-5xl w-full flex flex-row items-start lg:items-center">
+        Uh oh!
+      </h1>
+    </div>
+    <p class="font-bold text-2xl opacity-70 text-black text-center">We're sorry, but we couldn't find the page you were looking for.<br>Try checking the URL!</p>
+</section>
+</template>
+
+<script>
+export default {
+    layout: 'dashboard'
+}
+</script>
