94a101c2fb9455a12df3281d3fa6f7b9c09ddc63
Jim Bisenius <jim@neutroncreative.com>
2021-05-19 18:26:58 -0400

    Update styling on referral button

--- client/layouts/dashboard.vue
+++ client/layouts/dashboard.vue
@@ -8,7 +8,7 @@
           <img src="/Compass.svg" style="width: 16px;height:auto;"/>
           <input type="text" class="font-bold flex-grow flex-1 text-sm ml-2" style="background:transparent;" placeholder="Search pages, guides, and documentation..."/>
         </div>
-        <n-link to="/dashboard/referrals" class="py-1 px-4 rounded-full text-gdp bg-opaqueIndigo text-sm font-bold leading-tight mx-8 cursor-pointer flex items-center justify-center hover:bg-gdp hover:text-white">Refer a friend and get $10!</n-link>
+        <n-link to="/dashboard/referrals" class="py-1 px-4 rounded-full text-gdp bg-opaqueIndigo text-sm font-bold leading-tight mx-8 cursor-pointer flex items-center justify-center hover:border-gdp border-2 border-opaqueIndigo">Refer a friend and get $10!</n-link>
       </div>
       <div class="flex flex-col lg:flex-row w-full max-w-6xl h-screen oveflow-x-hidden">
         <div class="flex flex-col text-black font-semibold nav justify-start">
