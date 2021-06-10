aec9c6c9df2735280522736ee9ca7e5e3dd5a85a
Navid Kabir <navid113@gmail.com>
2021-06-10 17:10:54 -0400

    Further cleanup

--- api/tests/marketplace.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var query_utils_1 = require("../src/utils/query-utils");
-test("test values function", function () {
-    expect(query_utils_1.QueryUtils.values(5, 1)).toBe("($1,$2,$3,$4,$5)");
-    expect(query_utils_1.QueryUtils.values(3, 3)).toBe("($1,$2,$3), ($4,$5,$6), ($7,$8,$9)");
-});
