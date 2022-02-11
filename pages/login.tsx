654c5bbe83c2745108fcae957d976ad080977ef3
Jim Bisenius <jim@curri.com>
2022-02-11 00:00:46 -0800

    Clean up login styles

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,9 +13,9 @@ const Home: NextPage = () => {
 
       <div className='flex flex-col items-center justify-center w-full max-w-md p-8 rounded-xl bg-white shadow'>
       <div className='mb-6' style={{width: 44}}><Logo/></div>
-        <h1 className='font-bold text-3xl mb-4'>Login to your Singlelink</h1>
+        <h1 className='font-semibold text-2xl mb-4'>Login to your Singlelink</h1>
         <p className=' mb-6 text-gray-600'>Enter your credentials to access your account</p>
-        <input className='mb-4 px-5 text-lg py-3 rounded-lg border border-gray-200 w-full bg-white focus:ring-4 focus:ring-opacity-50 focus:ring-indigo-600 outline-0 ring-offset-2 focus:border-gray-1' type="password" placeholder="Password"/>
+        <input className='mb-4 px-5 py-3 rounded-lg border border-gray-200 w-full bg-white focus:ring-4 focus:ring-opacity-50 focus:ring-indigo-600 outline-0 ring-offset-2 focus:border-gray-1' type="password" placeholder="Password"/>
         <button className='px-8 py-4 text-xl bg-indigo-600 w-full text-white font-semibold rounded-xl hover:bg-indigo-700'>Log in</button>
       </div>
       <div className='text-gray-600 mt-12'>Forgot your password?<a className='ml-2 text-indigo-600 font-semibold hover:underline hover:text-indigo-700 cursor-pointer'>Reset password</a></div>
