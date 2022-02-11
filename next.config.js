e080003f78c0bfa1592cd394799307fbd7397a05
Jim Bisenius <jim@curri.com>
2022-02-11 03:48:12 -0800

    Move to EVs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PGUSER: process.env.PGUSER,
+    PGHOST: process.env.PGHOST,
+    PGDATABASE: process.env.PGDATABASE,
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGPORT: process.env.PGPORT,
+    GRAPHQL_URL: process.env.GRAPHQL_URL
+  }
 }
 
 module.exports = nextConfig
