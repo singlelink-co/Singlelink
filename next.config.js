1693d5d1d164a9a2bff9455394d2675351893daa
Jim Bisenius <jim@curri.com>
2022-02-11 03:40:02 -0800

    Replace details in next.config.js with env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GRAPHQL_URL: 'http://localhost:3000/api/graphql'
-  }
 }
 
 module.exports = nextConfig
