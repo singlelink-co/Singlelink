630d22a7a474bee687ab845ded706f1e44c2107d
Jim Bisenius <jim@curri.com>
2022-02-11 06:27:04 -0800

    Added more branding (optional)

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const nextConfig = {
     PGPASSWORD: process.env.PGPASSWORD,
     PGPORT: process.env.PGPORT,
     GRAPHQL_URL: process.env.GRAPHQL_URL,
-    META_TITLE: process.env.META_TITLE ?? 'Singlelink under construction',
-    META_DESC: process.env.META_DESC ?? 'Another Singlelink micro-site waiting to be built!',
+    META_TITLE: (process.env.BRANDING !== 'false') ? (process.env.META_TITLE ?? 'Under construction') + ' - Singlelink' : (process.env.META_TITLE ?? 'Under construction'),
+    META_DESC: (process.env.BRANDING !== 'false') ? (process.env.META_DESC ?? 'Another micro-site waiting to be built!') + ' Built with Singlelink.' : (process.env.META_DESC ?? 'Another micro-site waiting to be built!'),
     META_IMG: process.env.META_IMG ?? 'https://singlelink.co/og-image-updated.jpg',
-    BRANDING: (process.env.BRANDING !== 'false') ?? true
+    BRANDING: (process. env.BRANDING !== 'false') ?? true
   }
 }
 
