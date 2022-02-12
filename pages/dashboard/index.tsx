db6f2165e9703a0192226cd88b4c19cb33e66726
Jim Bisenius <jim@curri.com>
2022-02-12 01:33:16 -0800

    Force sharing row to stretch to width of parent

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -86,7 +86,7 @@ const Dashboard = () => {
                         <iframe width={347} height={767} src='https://beta.singlelink.co'></iframe>
                     </div>
                 </div>
-                <div className='flex flex-row items-center justify-center text-center p-4' style={{borderTop: 'solid 1px rgba(0,0,0,.15)'}}>
+                <div className='flex flex-row items-center justify-center w-full text-center p-4' style={{borderTop: 'solid 1px rgba(0,0,0,.15)'}}>
                     Share your Singlelink:
                     <a href="https://beta.singlelink.co" className='ml-2 mr-6 font-semibold text-indigo-600'>beta.singlelink.co</a>
                     <svg className="w-4 h-4 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
