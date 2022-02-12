2718397d01a751574293871c6ba8ccf6806ad8f8
Jim Bisenius <jim@curri.com>
2022-02-12 01:11:35 -0800

    Add phone preview

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -71,7 +71,7 @@ const Dashboard = () => {
                 </Link>
             </div>
             { /* Content */}
-            <div className='flex flex-col h-screen fixed top-0 right-0 bg-white' style={{width: 450, borderLeft: 'solid 1px rgba(0,0,0,0.15)'}}>
+            <div className='flex flex-col items-center justify-between h-screen fixed top-0 right-0 bg-white' style={{width: 450, borderLeft: 'solid 1px rgba(0,0,0,0.15)'}}>
                 <div className='flex flex-row items-center justify-between w-full px-4 py-6'  style={{borderBottom: 'solid 1px rgba(0,0,0,0.15)'}}>
                     <div className='flex flex-row items-center justify-start'>
                         <svg className="w-4 h-4 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
@@ -81,7 +81,12 @@ const Dashboard = () => {
                         <option>iPhone 13 Pro</option>
                     </select>
                 </div>
-                <div className='flex flex-row items-center justify-center text-center p-4 mt-auto' style={{borderTop: 'solid 1px rgba(0,0,0,.15)'}}>
+                <div className='flex items-center justify-center' style={{width:375, height:812, transform: 'scale(.8)', borderRadius: 65, backgroundColor: '#000'}}>
+                    <div style={{width:347, height: 767, borderRadius: 50, overflow: 'hidden'}}>
+                        <iframe width={347} height={767} src='https://beta.singlelink.co'></iframe>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center justify-center text-center p-4' style={{borderTop: 'solid 1px rgba(0,0,0,.15)'}}>
                     Share your Singlelink:
                     <a href="https://beta.singlelink.co" className='ml-2 mr-6 font-semibold text-indigo-600'>beta.singlelink.co</a>
                     <svg className="w-4 h-4 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
