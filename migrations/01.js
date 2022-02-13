023d0a676bf1f03b95a0ef2ae1a026e7edf4ea68
Jim Bisenius <jim@curri.com>
2022-02-13 04:36:10 -0800

    Fix await error in postinstall

--- migrations/01.js
+++ migrations/01.js
@@ -7,24 +7,28 @@ const client = new Pool({
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT
 })
+const migrate = async () => {
+    // Test DB Connection
+    await client.connect((err) => {
+        if (err) {
+            console.log('Singlelink: Database connection error!')
+            throw err;
+        }
+    });
+    // Run initial migration
+    console.log('Singlelink: Running migration 01.js')
+    await client.query(`
+        CREATE TABLE IF NOT EXISTS links (
+            id SERIAL,
+            label TEXT,
+            content TEXT,
+            type TEXT NOT NULL,
+            position INT UNIQUE NOT NULL
+        );
+    `)
+    console.log('Singlelink: Initial migration completed!')
+    // Exit post-install script
+    process.exit(0)
+}
 
-// Test DB Connection
-await client.connect((err) => {
-    if (err) {
-        console.log('Singlelink: Database connection error!')
-        throw err;
-    }
-});
-
-console.log('Singlelink: Running migration 01.js')
-await client.query(`
-    CREATE TABLE IF NOT EXISTS links (
-        id SERIAL,
-        label TEXT,
-        content TEXT,
-        type TEXT NOT NULL,
-        position INT UNIQUE NOT NULL
-    );
-`)
-console.log('Singlelink: Initial migration completed!')
-process.exit(0)
+migrate()
