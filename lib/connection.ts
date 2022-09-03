56a8ce198e778177eda0511120e0db0bfd4da8e5
Jim Bisenius <jim@curri.com>
2022-09-03 08:08:08 +0000

    Add DB URL support

--- lib/connection.ts
+++ lib/connection.ts
@@ -1,12 +1,14 @@
 const mysql = require('mysql2/promise')
 
-const client = mysql.createPool({
-    user: process.env.DB_USER ?? 'root',
-    host: process.env.DB_HOST ?? '127.0.0.1',
-    database: process.env.DB_DATABASE ?? 'singlelink',
-    password: process.env.DB_PASSWORD ?? 'single-my-links',
-    port: process.env.DB_PORT ?? 3306
-})
+const client = mysql.createPool(
+    process.env.DB_URL ?? {
+        user: process.env.DB_USER ?? 'root',
+        host: process.env.DB_HOST ?? '127.0.0.1',
+        database: process.env.DB_DATABASE ?? 'singlelink',
+        password: process.env.DB_PASSWORD ?? 'single-my-links',
+        port: process.env.DB_PORT ?? 3306
+    }
+)
 
 // TODO: Add error handling if DB connection fails
 
