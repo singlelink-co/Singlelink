360ebb6b51d93962176421ef11cfd05e27bc8a50
Jim Bisenius <jim@curri.com>
2022-09-03 08:14:51 +0000

    Add planetscale support

--- migrations/0001-create-links.sql
+++ migrations/0001-create-links.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS links (
-    id SERIAL,
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     label TEXT,
     content TEXT,
     type TEXT NOT NULL,
--- migrations/0002-create-events.sql
+++ migrations/0002-create-events.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS events (
-    id SERIAL,
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type ENUM('view', 'click'),
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     link_id INT REFERENCES links (id)
