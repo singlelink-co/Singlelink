7906342439cf2b3851d9a56aad1388b82cb7dddd
Jim Bisenius <jim@curri.com>
2022-09-03 07:17:39 +0000

    Old dependencies pruned

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    PGUSER: process.env.PGUSER,
-    PGHOST: process.env.PGHOST,
-    PGDATABASE: process.env.PGDATABASE,
-    PGPASSWORD: process.env.PGPASSWORD,
-    PGPORT: process.env.PGPORT,
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
     META_TITLE: (process.env.BRANDING !== 'false') ? (process.env.META_TITLE ?? 'Under construction') + ' - Singlelink' : (process.env.META_TITLE ?? 'Under construction'),
     META_DESC: (process.env.BRANDING !== 'false') ? (process.env.META_DESC ?? 'Another micro-site waiting to be built!') + ' Built with Singlelink.' : (process.env.META_DESC ?? 'Another micro-site waiting to be built!'),
-    META_IMG: process.env.META_IMG ?? 'https://singlelink.co/og-image-updated.jpg',
+    META_IMG: process.env.META_IMG ?? 'https://singlelink.co/og-image-updated.jDB_',
     BRANDING: (process. env.BRANDING !== 'false') ?? true,
     SECRET: process.env.SECRET,
     PASSWORD: process.env.PASSWORD
